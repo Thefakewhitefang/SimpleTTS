9f24a21f953e72893d924916d4fa664c115ad48b
Thefakewhitefang <121751769+Thefakewhitefang@users.noreply.github.com>
2023-01-02 17:15:14 +0530

    Update Form2.vb
    Added comments and other links

--- Form2.vb
+++ Form2.vb
@@ -1,6 +1,15 @@
-﻿Public Class Form2
-
+Public Class Form2
+    'Doesn't need an explanation
+    'It opens withe the default app
     Private Sub LinkLabel1_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
         System.Diagnostics.Process.Start("https://www.flaticon.com/authors/jessigue")
+    End Sub
+
+    Private Sub LinkLabel2_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        System.Diagnostics.Process.Start("https://github.com/Thefakewhitefang/SimpleTTS")
+    End Sub
+
+    Private Sub PictureBox1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox1.Click
+        System.Diagnostics.Process.Start("https://creativecommons.org/licenses/by-nc-sa/4.0/")
     End Sub
 End Class
